b3c79510798af0367d735714bae932b9f9d65071
Caryssa <cyperez8@gmail.com>
2016-12-04 20:32:06 +0000

    This is an auto-commit

--- js/favorites.js
+++ js/favorites.js
@@ -1,18 +1,18 @@
 function createFavorite() {
     var listOfFavorites = document.getElementsByClassName("favorite");
-    var theFirstFavorite = listOfFavorites.item(0);
-    
-    var theNewFavorite = theFirstFavorite.cloneNode(true);
-    
-    document.getElementById("content").appendChild(theNewFavorite);
-    
-    var theFavoriteTitle = document.getElementById("creator-title").value;
-    var theFavoriteDescription = document.getElementById("creator-description").value;
-    var theFavoriteMedia = document.getElementById("creator-media").value;
-    
-    theNewFavorite.getElementsByClassName("title")[0].innerHTML = theFavoriteTitle;
-    theNewFavorite.getElementsByClassName("description")[0].innerHTML = theFavoriteDescription;
-    theNewFavorite.getElementsByClassName("media")[0].innerHTML = theFavoriteMedia;
+      var theFirstFavorite = listOfFavorites.item(0);
+        var theNewFavorite = theFirstFavorite.cloneNode(true);
+           document.getElementById("content").appendChild(theNewFavorite);
+  var newFavoriteTitle = document.getElementById("creator-title").value;
+    var newFavoriteDescription = document.getElementById("creator-description").value;
 
-    
+console.log("The title of the new favorite is " + newFavoriteTitle);
+console.log("The description of the new favorite is " + newFavoriteDescription);
+   var newFavoriteTitle = document.getElementById("creator-title").value;
+    var newFavoriteDescription = document.getElementById("creator-description").value;
+     var newFavoriteMedia = document.getElementById("creator-media").value;
+    theNewFavorite.getElementsByClassName("title")[0].innerHTML = newFavoriteTitle;
+    theNewFavorite.getElementsByClassName("description")[0].innerHTML = newFavoriteDescription;
+    theNewFavorite.getElementsByClassName("media")[0].innerHTML = newFavoriteMedia;
+ console.log(listOfFavorites);
 }
